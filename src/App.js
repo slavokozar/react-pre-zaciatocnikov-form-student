84137021bdfffcba75b9118d9c0b8eaac348bea2
Slavo Kozar <slavo.kozar@gmail.com>
2022-03-22 00:31:20 +0100

    added form

--- src/App.js
+++ src/App.js
@@ -61,100 +61,104 @@ function App() {
     const [stage, setStage] = useState('');
     const [distance, setDistance] = useState('');
     const [message, setMessage] = useState('');
-    
+
     return (
         <div className="container p-3">
             <h1 className="mb-3">Formulár Študenta</h1>
             <div className="row">
                 <div className="col">
-                    <div className="mb-1">
-                        <label className="form-label" htmlFor="name">Meno</label>
-                        <input className="form-control" type="text" name="name" id="name" value={name}
-                               onChange={(e) => setName(e.target.value)}/>
-                    </div>
-                    <div className="mb-1">
-                        <label className="form-label" htmlFor="email">Email</label>
-                        <input className="form-control" type="email" name="email" id="email" value={email}
-                               onChange={(e) => setEmail(e.target.value)}/>
-                    </div>
-                    <div className="mb-1">
-                        <label className="form-label" htmlFor="year">Ročník narodenia</label>
-                        <input className="form-control" type="number" name="year" id="year" value={year}
-                               onChange={(e) => setYear(e.target.value)}/>
-                    </div>
-                    <div className="mb-1">
-                        <label className="form-label" htmlFor="school">Univerzita</label>
-                        <select className="form-control" value={university}
-                                onChange={(e) => setUniversity(e.target.value)}>
-                            <option value="">Prosím vyberte univerzitu</option>
-                            <option value="ČVUT">ČVUT</option>
-                            <option value="UK">Univerzita Karlova</option>
-                            <option value="ČZU">Česká Zemědelská Univerzita</option>
-                            <option value="VŠE">Vysoká Škola Ekonomická</option>
-                        </select>
-                    </div>
-                    <div className="mb-1">
-                        <label className="form-label" htmlFor="faculty">Fakulta</label>
-                        <select className="form-control" value={faculty} onChange={(e) => setFaculty(e.target.value)}>
-                            {
-                                university === '' ? (
-                                    <option value="">Prosím vyberte najprv univerzitu</option>
-                                ) : (
-                                    <>
-                                        <option value="">Prosím vyberte fakultu</option>
-                                        {faculties[university]}
-                                    </>
-                                )
-                            }
-                        </select>
-                    </div>
-                    <div className="mb-1">
-                        <p>Etapa štúdia</p>
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="stage" id="bachelor" value="bachelor"
-                                   checked={stage === 'bachelor'}
+                    <form action="" onSubmit={(e) => {e.preventDefault(); alert(e)}}>
+                        <div className="mb-1">
+                            <label className="form-label" htmlFor="name">Meno</label>
+                            <input className="form-control" type="text" name="name" id="name" value={name}
+                                   onChange={(e) => setName(e.target.value)}/>
+                        </div>
+                        <div className="mb-1">
+                            <label className="form-label" htmlFor="email">Email</label>
+                            <input className="form-control" type="email" name="email" id="email" value={email}
+                                   onChange={(e) => setEmail(e.target.value)}/>
+                        </div>
+                        <div className="mb-1">
+                            <label className="form-label" htmlFor="year">Ročník narodenia</label>
+                            <input className="form-control" type="number" name="year" id="year" value={year}
+                                   onChange={(e) => setYear(e.target.value)}/>
+                        </div>
+                        <div className="mb-1" style={{padding: '100px'}}>
+                            <label className="form-label" htmlFor="school">Univerzita</label>
+                            <select className="form-control" value={university}
+                                    onChange={(e) => setUniversity(e.target.value)}>
+                                <option value="">Prosím vyberte univerzitu</option>
+                                <option value="ČVUT">ČVUT</option>
+                                <option value="UK">Univerzita Karlova</option>
+                                <option value="ČZU">Česká Zemědelská Univerzita</option>
+                                <option value="VŠE">Vysoká Škola Ekonomická</option>
+                            </select>
+                        </div>
+                        <div className="mb-1">
+                            <label className="form-label" htmlFor="faculty">Fakulta</label>
+                            <select className="form-control" value={faculty}
+                                    onChange={(e) => setFaculty(e.target.value)}>
+                                {
+                                    university === '' ? (
+                                        <option value="">Prosím vyberte najprv univerzitu</option>
+                                    ) : (
+                                        <>
+                                            <option value="">Prosím vyberte fakultu</option>
+                                            {faculties[university]}
+                                        </>
+                                    )
+                                }
+                            </select>
+                        </div>
+                        <div className="mb-1">
+                            <p>Etapa štúdia</p>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="stage" id="bachelor"
+                                       value="bachelor"
+                                       checked={stage === 'bachelor'}
+                                       onChange={(e) => {
+                                           setStage(e.target.checked ? 'bachelor' : stage)
+                                       }}
+                                />
+                                <label className="form-check-label" htmlFor="bachelor">Bakalárska</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="stage" value="master" id="master"
+                                       checked={stage === 'master'}
+                                       onChange={(e) => {
+                                           setStage(e.target.checked ? 'master' : stage)
+                                       }}
+                                />
+                                <label className="form-check-label" htmlFor="master">Magisterská</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="stage" value="doctor" id="doctor"
+                                       checked={stage === 'doctor'}
+                                       onChange={(e) => {
+                                           setStage(e.target.checked ? 'doctor' : stage)
+                                       }}
+                                />
+                                <label className="form-check-label" htmlFor="doctor">Doktorská</label>
+                            </div>
+                        </div>
+                        <div className="form-check mb-1">
+                            <input className="form-check-input" type="checkbox" name="distance" id="distance"
+                                   checked={distance}
                                    onChange={(e) => {
-                                       setStage(e.target.checked ? 'bachelor' : stage)
+                                       setDistance(e.target.checked)
                                    }}
                             />
-                            <label className="form-check-label" htmlFor="bachelor">Bakalárska</label>
+                            <label className="form-check-label" htmlFor="distance">
+                                Distancne studium
+                            </label>
                         </div>
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="stage" value="master" id="master"
-                                   checked={stage === 'master'}
-                                   onChange={(e) => {
-                                       setStage(e.target.checked ? 'master' : stage)
-                                   }}
-                            />
-                            <label className="form-check-label" htmlFor="master">Magisterská</label>
+                        <div className="mb-1">
+                            <label className="form-label" htmlFor="message">Správa (max 200 znakov)</label>
+                            <textarea className="form-control" name="message" id="message" cols="30" rows="10"
+                                      value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
                         </div>
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="stage" value="doctor" id="doctor"
-                                   checked={stage === 'doctor'}
-                                   onChange={(e) => {
-                                       setStage(e.target.checked ? 'doctor' : stage)
-                                   }}
-                            />
-                            <label className="form-check-label" htmlFor="doctor">Doktorská</label>
-                        </div>
-                    </div>
-                    <div className="form-check mb-1">
-                        <input className="form-check-input" type="checkbox" name="distance" id="distance"
-                               checked={distance}
-                               onChange={(e) => {
-                                   setDistance(e.target.checked)
-                               }}
-                        />
-                        <label className="form-check-label" htmlFor="distance">
-                            Distancne studium
-                        </label>
-                    </div>
-                    <div className="mb-1">
-                        <label className="form-label" htmlFor="message">Správa (max 200 znakov)</label>
-                        <textarea className="form-control" name="message" id="message" cols="30" rows="10"
-                                  value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-                    </div>
-                    <button className="btn btn-primary" type="submit">Odoslať</button>
+                        <button className="btn btn-primary" type="submit">Odoslať</button>
+                    </form>
                 </div>
                 <div className="col" style={{backgroundColor: '#f8f9fa', padding: '1rem'}}>
                     <div>
