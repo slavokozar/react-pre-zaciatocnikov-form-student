fadd1182d3d8451bfa97ef1031f7fe3f09ec0a36
Slavo Kozar <slavo.kozar@gmail.com>
2022-03-22 18:08:09 +0100

    template for exercises

--- src/App.js
+++ src/App.js
@@ -30,7 +30,8 @@ const faculties = {
         <option value="Fakulta stavební (FSv)">Fakulta stavební (FSv)</option>,
         <option value="Fakulta strojní (FS)">Fakulta strojní (FS)</option>,
         <option value="Fakulta elektrotechnická (FEL)">Fakulta elektrotechnická (FEL)</option>,
-        <option value="Fakulta jaderná a fyzikálně inženýrská (FJFI)">Fakulta jaderná a fyzikálně inženýrská (FJFI)</option>,
+        <option value="Fakulta jaderná a fyzikálně inženýrská (FJFI)">Fakulta jaderná a fyzikálně inženýrská
+            (FJFI)</option>,
         <option value="Fakulta architektury (FA)">Fakulta architektury (FA)</option>,
         <option value="Fakulta dopravní (FD)">Fakulta dopravní (FD)</option>,
         <option value="Fakulta biomedicínského inženýrství (FBMI)">Fakulta biomedicínského inženýrství (FBMI)</option>,
@@ -38,7 +39,8 @@ const faculties = {
     ],
     "ČZU": [
         <option value="Provozně ekonomická fakulta">Provozně ekonomická fakulta</option>,
-        <option value="Fakulta agrobiologie, potravinových a přírodních zdrojů">Fakulta agrobiologie, potravinových a přírodních zdrojů</option>,
+        <option value="Fakulta agrobiologie, potravinových a přírodních zdrojů">Fakulta agrobiologie, potravinových a
+            přírodních zdrojů</option>,
         <option value="Technická fakulta">Technická fakulta</option>,
         <option value="Fakulta životního prostředí">Fakulta životního prostředí</option>,
         <option value="Fakulta lesnická a dřevařská">Fakulta lesnická a dřevařská</option>,
@@ -53,60 +55,66 @@ function App() {
             <h1 className="mb-3">Formulár Študenta</h1>
             <div className="row">
                 <div className="col">
-                    <div className="mb-1">
-                        <label className="form-label" htmlFor="name">Meno</label>
-                        <input className="form-control" type="text" name="name" id="name"/>
-                    </div>
-                    <div className="mb-1">
-                        <label className="form-label" htmlFor="email">Email</label>
-                        <input className="form-control" type="email" name="email" id="email"/>
-                    </div>
-                    <div className="mb-1">
-                        <label className="form-label" htmlFor="year">Ročník narodenia</label>
-                        <input className="form-control" type="number" name="year" id="year"/>
-                    </div>
-                    <div className="mb-1">
-                        <label className="form-label" htmlFor="school">Univerzita</label>
-                        <select className="form-control">
-                            <option value="">Prosím vyberte univerzitu</option>
-                            <option value="CVUT">ČVUT</option>
-                            <option value="UK">Univerzita Karlova</option>
-                            <option value="CZU">Česká Zemědelská Univerzita</option>
-                            <option value="VSE">Vysoká Škola Ekonomická</option>
-                        </select>
-                    </div>
-                    <div className="mb-1">
-                        <label className="form-label" htmlFor="faculty">Fakulta</label>
-                        <select className="form-control">
-                            <option value="">Prosím vyberte fakultu</option>
-                        </select>
-                    </div>
-                    <div className="mb-1">
-                        <p>Etapa štúdia</p>
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="stage" id="bachelor" value="bachelor"/>
-                            <label className="form-check-label" htmlFor="bachelor">Bakalárska</label>
+                    <form action="">
+                        <div className="mb-1">
+                            <label className="form-label" htmlFor="name">Meno</label>
+                            <input className="form-control" type="text" name="name" id="name"/>
                         </div>
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="stage" value="master" id="master"/>
-                            <label className="form-check-label" htmlFor="master">Magisterská</label>
+                        <div className="mb-1">
+                            <label className="form-label" htmlFor="email">Email</label>
+                            <input className="form-control" type="email" name="email" id="email"/>
                         </div>
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="stage" value="doctor" id="doctor"/>
-                            <label className="form-check-label" htmlFor="doctor">Doktorská</label>
+                        <div className="mb-1">
+                            <label className="form-label" htmlFor="year">Ročník narodenia</label>
+                            <input className="form-control" type="number" name="year" id="year"/>
                         </div>
-                    </div>
-                    <div className="form-check mb-1">
-                        <input className="form-check-input" type="checkbox" name="distance" id="distance"/>
-                        <label className="form-check-label" htmlFor="distance">
-                            Distancne studium
-                        </label>
-                    </div>
-                    <div className="mb-1">
-                        <label className="form-label" htmlFor="message">Správa (max 200 znakov)</label>
-                        <textarea className="form-control" name="message" id="message" cols="30" rows="10"></textarea>
-                    </div>
-                    <button className="btn btn-primary" type="submit">Odoslať</button>
+                        <div className="mb-1">
+                            <label className="form-label" htmlFor="school">Univerzita</label>
+                            <select className="form-control">
+                                <option value="">Prosím vyberte univerzitu</option>
+                                <option value="CVUT">ČVUT</option>
+                                <option value="UK">Univerzita Karlova</option>
+                                <option value="CZU">Česká Zemědelská Univerzita</option>
+                                <option value="VSE">Vysoká Škola Ekonomická</option>
+                            </select>
+                        </div>
+                        <div className="mb-1">
+                            <label className="form-label" htmlFor="faculty">Fakulta</label>
+                            <select className="form-control">
+                                <option value="">Prosím vyberte fakultu</option>
+                            </select>
+                        </div>
+                        <div className="mb-1">
+                            <p>Etapa štúdia</p>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="stage" id="bachelor"
+                                       value="bachelor"/>
+                                <label className="form-check-label" htmlFor="bachelor">Bakalárska</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="stage" value="master"
+                                       id="master"/>
+                                <label className="form-check-label" htmlFor="master">Magisterská</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="stage" value="doctor"
+                                       id="doctor"/>
+                                <label className="form-check-label" htmlFor="doctor">Doktorská</label>
+                            </div>
+                        </div>
+                        <div className="form-check mb-1">
+                            <input className="form-check-input" type="checkbox" name="distance" id="distance"/>
+                            <label className="form-check-label" htmlFor="distance">
+                                Distancne studium
+                            </label>
+                        </div>
+                        <div className="mb-1">
+                            <label className="form-label" htmlFor="message">Správa (max 200 znakov)</label>
+                            <textarea className="form-control" name="message" id="message" cols="30"
+                                      rows="10"></textarea>
+                        </div>
+                        <button className="btn btn-primary" type="submit">Odoslať</button>
+                    </form>
                 </div>
                 <div className="col" style={{backgroundColor: '#f8f9fa', padding: '1rem'}}>
                     <div>
