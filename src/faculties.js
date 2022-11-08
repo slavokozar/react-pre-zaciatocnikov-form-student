5a23f9659ba9e67e3621cc212b5a09d2924ff296
Slavo Kozar <slavo.kozar@gmail.com>
2022-11-08 17:42:52 +0100

    faculties moved to separate file

---
+++ src/faculties.js
@@ -0,0 +1,52 @@
+const faculties = {
+    "UK": [
+        <option value="Katolická teologická fakulta">Katolická teologická fakulta</option>,
+        <option value="Evangelická teologická fakulta">Evangelická teologická fakulta</option>,
+        <option value="Husitská teologická fakulta">Husitská teologická fakulta</option>,
+        <option value="Právnická fakulta">Právnická fakulta</option>,
+        <option value="1. lékařská fakulta">1. lékařská fakulta</option>,
+        <option value="2. lékařská fakulta">2. lékařská fakulta</option>,
+        <option value="3. lékařská fakulta">3. lékařská fakulta</option>,
+        <option value="Lékařská fakulta v Plzni">Lékařská fakulta v Plzni</option>,
+        <option value="Lékařská fakulta v Hradci Králové">Lékařská fakulta v Hradci Králové</option>,
+        <option value="Farmaceutická fakulta v Hradci Králové">Farmaceutická fakulta v Hradci Králové</option>,
+        <option value="Filozofická fakulta">Filozofická fakulta</option>,
+        <option value="Přírodovědecká fakulta">Přírodovědecká fakulta</option>,
+        <option value="Matematicko-fyzikální fakulta">Matematicko-fyzikální fakulta</option>,
+        <option value="Pedagogická fakulta">Pedagogická fakulta</option>,
+        <option value="Fakulta sociálních věd">Fakulta sociálních věd</option>,
+        <option value="Fakulta tělesné výchovy a sportu">Fakulta tělesné výchovy a sportu</option>,
+        <option value="Fakulta humanitních studií">Fakulta humanitních studií</option>,
+    ],
+    "VŠE": [
+        <option value="Fakulta financí a účetnictví">Fakulta financí a účetnictví</option>,
+        <option value="Fakulta mezinárodních vztahů">Fakulta mezinárodních vztahů</option>,
+        <option value="Fakulta podnikohospodářská">Fakulta podnikohospodářská</option>,
+        <option value="Fakulta informatiky a statistiky">Fakulta informatiky a statistiky</option>,
+        <option value="Národohospodářská fakulta">Národohospodářská fakulta</option>,
+        <option value="Fakulta managementu">Fakulta managementu</option>
+    ],
+    "ČVUT": [
+        <option value="Fakulta stavební (FSv)">Fakulta stavební (FSv)</option>,
+        <option value="Fakulta strojní (FS)">Fakulta strojní (FS)</option>,
+        <option value="Fakulta elektrotechnická (FEL)">Fakulta elektrotechnická (FEL)</option>,
+        <option value="Fakulta jaderná a fyzikálně inženýrská (FJFI)">Fakulta jaderná a fyzikálně inženýrská
+            (FJFI)</option>,
+        <option value="Fakulta architektury (FA)">Fakulta architektury (FA)</option>,
+        <option value="Fakulta dopravní (FD)">Fakulta dopravní (FD)</option>,
+        <option value="Fakulta biomedicínského inženýrství (FBMI)">Fakulta biomedicínského inženýrství (FBMI)</option>,
+        <option value="Fakulta informačních technologií (FIT)">Fakulta informačních technologií (FIT)</option>
+    ],
+    "ČZU": [
+        <option value="Provozně ekonomická fakulta">Provozně ekonomická fakulta</option>,
+        <option value="Fakulta agrobiologie, potravinových a přírodních zdrojů">Fakulta agrobiologie, potravinových a
+            přírodních zdrojů</option>,
+        <option value="Technická fakulta">Technická fakulta</option>,
+        <option value="Fakulta životního prostředí">Fakulta životního prostředí</option>,
+        <option value="Fakulta lesnická a dřevařská">Fakulta lesnická a dřevařská</option>,
+        <option value="Fakulta tropického zemědělství">Fakulta tropického zemědělství</option>,
+        <option value="Institut vzdělávání a poradenství">Institut vzdělávání a poradenství</option>,
+    ]
+}
+
+export default faculties;
